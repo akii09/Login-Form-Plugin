95aecd5d704af6564f0d59a5f7ed8b0009ad7356
Akash <akashpise588@gmail.com>
2021-11-10 09:36:44 +0530

    uploaded

---
+++ login-form-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package login-form-plugin
+ * Plugin Name: Login Form Plugin
+ * Plugin URI: https: //github.com/akii09/Login-Form-Plugin
+ * Description: A Login Form Plugin for Wordpress.
+ * Author: Brainstorm Force -Akii
+ * Version: 1.0.0
+ * Text Domain: login-form-plugin
+ * Domain Path: /languages
+ * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
